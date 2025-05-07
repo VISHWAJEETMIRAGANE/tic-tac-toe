72df4c074ebf8533a773af8dfab540c2014c9dfb
vishwajeetmiragane <vishwajeetmirgane@gmail.com>
2025-05-08 01:12:14 +0530

    main.py

--- main.py
+++ main.py
@@ -1,60 +1,51 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request
+import os
 
 app = Flask(__name__)
 
-class TicTacToe:
+class TicTacToeGame:
     def __init__(self):
         self.board = [' ' for _ in range(9)]
-        self.current_winner = None
+        self.current_player = 'X'
 
-    def print_board(self):
-        rows = [self.board[i*3:(i+1)*3] for i in range(3)]
-        return rows
-
-    def available_moves(self):
-        return [i for i, x in enumerate(self.board) if x == ' ']
-
-    def play(self, square, letter):
-        if self.board[square] == ' ':
-            self.board[square] = letter
-            if self.check_winner(square, letter):
-                self.current_winner = letter
+    def make_move(self, position):
+        if self.board[position] == ' ':
+            self.board[position] = self.current_player
+            self.current_player = 'O' if self.current_player == 'X' else 'X'
             return True
         return False
 
-    def check_winner(self, square, letter):
-        row_ind = square // 3
-        row = self.board[row_ind*3:(row_ind+1)*3]
-        if all([spot == letter for spot in row]):
-            return True
+    def get_board(self):
+        return self.board
 
-        col_ind = square % 3
-        col = [self.board[col_ind+i*3] for i in range(3)]
-        if all([spot == letter for spot in col]):
-            return True
+    def check_winner(self):
+        winning_combinations = [(0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6)]
+        for a, b, c in winning_combinations:
+            if self.board[a] == self.board[b] == self.board[c] != ' ':
+                return self.board[a]
+        if ' ' not in self.board:
+            return 'Draw'
+        return None
 
-        if square % 2 == 0:
-            diagonal1 = [self.board[i] for i in [0, 4, 8]]
-            if all([spot == letter for spot in diagonal1]):
-                return True
-            diagonal2 = [self.board[i] for i in [2, 4, 6]]
-            if all([spot == letter for spot in diagonal2]):
-                return True
-        return False
+    def reset_game(self):
+        self.board = [' ' for _ in range(9)]
+        self.current_player = 'X'
 
-@app.route("/")
+game = TicTacToeGame()
+
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    game = TicTacToe()
-    return render_template("index.html", board=game.print_board(), winner=None)
+    if request.method == 'POST':
+        move = request.form.get('move')
+        if move and move.isdigit():
+            move = int(move) - 1
+            game.make_move(move)
 
-@app.route("/play", methods=["POST"])
-def play():
-    square = int(request.form.get("square"))
-    letter = request.form.get("letter")
-    game = TicTacToe()
-    game.play(square, letter)
-    winner = game.current_winner
-    return render_template("index.html", board=game.print_board(), winner=winner)
+    winner = game.check_winner()
+    board = game.get_board()
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    return render_template('index.html', board=board, winner=winner)
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 8081))
+    app.run(debug=True, host='0.0.0.0', port=port)
